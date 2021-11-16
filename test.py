b37781959e3995a41ffb30d1e380f40f58b68e63
Flozad <flozada01@gmail.com>
2021-11-16 10:43:10 -0300

    1.0.0

---
+++ test.py
@@ -0,0 +1,25 @@
+from transformers import pipeline
+
+# generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
+
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B')
+#generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
+
+prompt = 'La automatización es una característica esencial hoy en dia'
+prompt1 = 'El tiempo es limitado y debemos cuidarlo'
+prompt2 = 'Un robot es un aparato'
+prompt3 = 'Telegram es una plataforma que permite'
+prompt4 = 'Hoy más que nunca tenemos que aprovechar los robots para'
+
+res = generator(prompt, max_length=300, do_sample=True, temperature=0.8, min_length=150)
+res1 = generator(prompt1, max_length=300, do_sample=True, temperature=0.8, min_length=150)
+res2 = generator(prompt2, max_length=300, do_sample=True, temperature=0.8, min_length=150)
+res3 = generator(prompt3, max_length=300, do_sample=True, temperature=0.8, min_length=150)
+res4 = generator(prompt4, max_length=300, do_sample=True, temperature=0.8, min_length=150)
+
+with open("Output.txt", "w") as text_file:
+    text_file.write(res[0]['generated_text'])
+    text_file.write(res1[0]['generated_text'])
+    text_file.write(res2[0]['generated_text'])
+    text_file.write(res3[0]['generated_text'])
+    text_file.write(res4[0]['generated_text'])
